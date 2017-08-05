62f26602b4ce6957ea1a224ca87319828bbdb4a9
peter <peterwilson.intake34@gmail.com>
2017-08-05 17:26:50 +0200

    UI of home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import * as $ from 'jquery';
 
+import { OwlModule } from 'ngx-owl-carousel';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
