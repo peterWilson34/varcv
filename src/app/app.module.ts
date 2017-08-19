97cfa1ae45e91d0baa90ce79aa5dc177935cd1c1
peter <peterwilson.intake34@gmail.com>
2017-08-19 12:10:11 +0200

    cv pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,46 @@ import * as $ from 'jquery';
 import { OwlModule } from 'ngx-owl-carousel';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { CvComponent } from './cv/cv.component';
+import { PersonalInfoComponent } from './cv/personal-info/personal-info.component';
+import { LanguagesComponent } from './cv/languages/languages.component';
+import { TechnicalSkillsComponent } from './cv/technical-skills/technical-skills.component';
+import { EducationComponent } from './cv/education/education.component';
+import { WorkExperienceComponent } from './cv/work-experience/work-experience.component';
+import { CoursesComponent } from './cv/courses/courses.component';
+import { TemplateComponent } from './cv/template/template.component';
+import { ObjectivesComponent } from './cv/objectives/objectives.component';
 
 const appRoutes:Routes=[
   {path:'',component:HomeComponent},
   {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent}
+  {path:'signup',component:SignupComponent},
+  {path:'cv',component:CvComponent,children:[
+    {path:'personal-info',component:PersonalInfoComponent},
+    {path:'languages',component:LanguagesComponent},
+    {path:'technical-skills',component:TechnicalSkillsComponent},
+    {path:'education',component:EducationComponent},
+    {path:'objectives',component:ObjectivesComponent},
+    {path:'work-experience',component:WorkExperienceComponent},
+    {path:'courses',component:CoursesComponent},
+    {path:'template',component:TemplateComponent}
+  ]}
 ];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    CvComponent,
+    PersonalInfoComponent,
+    LanguagesComponent,
+    TechnicalSkillsComponent,
+    EducationComponent,
+    WorkExperienceComponent,
+    CoursesComponent,
+    TemplateComponent,
+    ObjectivesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cv/cv.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-cv',
+  templateUrl: './cv.component.html',
+  styleUrls: ['./cv.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
